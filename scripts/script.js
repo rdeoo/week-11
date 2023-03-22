d05bdf9f7782df80cec57d362bb577b2a87f2ea4
Rohan Deo <rohandeo307@gmail.com>
2023-03-22 12:35:37 -0700

    default parameter

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,13 @@
 //   console.log(`hello ${name}`);
 // }
 // Greet("rohan");
-const Greet = (name)=> {
-    console.log(`hello ${name}`);
-  }
-  Greet("rohan");
+// const Greet = (name)=> {
+//     console.log(`hello ${name}`);
+//   }
+//   Greet("rohan");
+
+
+function Greet(name,time = "Day") {
+  console.log(`Good ${time} ${name}`);
+}
+Greet("rohan");

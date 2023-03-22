04437ce3f49e4ab29243336f9ea1f6b2cf1d5de4
Rohan Deo <rohandeo307@gmail.com>
2023-03-22 13:45:29 -0700

    Add dom elements activity

--- scripts/script.js
+++ scripts/script.js
@@ -7,24 +7,32 @@
 //   }
 //   Greet("rohan");
 
-
 // function Greet(name,time = "Day") {
 //   console.log(`Good ${time} ${name}`);
 // }
 // Greet("rohan");
 
-function circleArea(radius)
-{   if (isNaN(radius))
-    {
-    alert("this not a number");
+const radiusPara = document.querySelector("#radius");
+console.log(radiusPara);
+const resultPara = document.querySelector("#result");
+console.log(resultPara);
+
+function circleArea(radius) {
+  if (isNaN(radius)) {
+    resultPara.textContent = "This is not a number"
+    // alert("this not a number");
     // returns undefinded by default
-}else{
-    const area = Math.PI * radius **2;
-    return area
+  } else {
+    const area = Math.PI * radius ** 2;
+    return area;
+  }
 }
-}
-let userValue = prompt("Enter a number")
+let userValue = prompt("Enter a number");
+radiusPara.textContent = radiusPara.textContent +=userValue
 // prompt always returns a string
-let result = circleArea(userValue)
-alert(`The area of a cricle with radius ${userValue} is ${result.toFixed(2)}`)
-circleArea()
+let result = circleArea(userValue);
+if (result != undefined) {
+  resultPara.textContent = 
+    `The area of a cricle with radius ${userValue} is ${result.toFixed(2)}`
+  ;
+}

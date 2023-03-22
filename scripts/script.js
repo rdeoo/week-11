40e92b9ea44b01de0c41c44f9a88565048c79942
Rohan Deo <rohandeo307@gmail.com>
2023-03-22 12:56:02 -0700

    return statememt

--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,23 @@
 //   Greet("rohan");
 
 
-function Greet(name,time = "Day") {
-  console.log(`Good ${time} ${name}`);
+// function Greet(name,time = "Day") {
+//   console.log(`Good ${time} ${name}`);
+// }
+// Greet("rohan");
+
+function circleArea(radius)
+{   if (isNaN(radius))
+    {
+    alert("this not a number");
+    // returns undefinded by default
+}else{
+    const area = Math.PI * radius **2;
+    return area
 }
-Greet("rohan");
+}
+let userValue = prompt("Enter a number")
+// prompt always returns a string
+let result = circleArea(userValue)
+alert(`The area of a cricle with radius ${userValue} is ${result.toFixed(2)}`)
+circleArea()
